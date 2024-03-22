05698b9bb6f0f8179c04f573bf228fbf35f71055
binyag <bincbhnhi@gmail.com>
2024-03-22 11:35:35 +0200

    Update Clustering.py

--- Clustering.py
+++ Clustering.py
@@ -16,6 +16,7 @@ from tslearn.clustering import TimeSeriesKMeans
 from tslearn.preprocessing import TimeSeriesScalerMinMax  # Consider normalization
 from sklearn.decomposition import PCA
 from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
 
 
 def try_parse_date(date_str):
@@ -31,6 +32,7 @@ def try_parse_date(date_str):
             return pd.to_datetime(date_str, format="%m %d %Y")
 
 
+data.columns
 
 
 url = "https://github.com/binyag/Cities_Weather_Clustering/raw/main/data.csv"
@@ -46,26 +48,29 @@ df["Visibility"].fillna(df["Visibility"].median(), inplace=True)
 df["Cloud Cover"].fillna(0, inplace=True)
 
 df['Wind Direction'].fillna(df.groupby('Address')['Wind Direction'].transform('median'), inplace=True)
-
+x_w = df['Wind Direction'].apply(lambda x: np.cos(x * np.pi / 180))
+y_w = df['Wind Direction'].apply(lambda x: np.sin(x * np.pi / 180))
+df['vector_Wind_Direction'] = list(zip(x_w, y_w))
 # החלת הפונקציה על עמודה "תאריך"
 df["Date time"] = df["Date time"].apply(try_parse_date)
 #df = df.set_index('Date time')
 
 
-df["Rain"] = np.where(df["Conditions"].str.contains("Rain"), 1, 0)
-df["Partially cloudy"] = np.where(df["Conditions"].str.contains("Partially cloudy"), 1, 0)
-df["Overcast"] = np.where(df["Conditions"].str.contains("Overcast"), 1, 0)
+#df["Rain"] = np.where(df["Conditions"].str.contains("Rain"), 1, 0)
+#df["Partially cloudy"] = np.where(df["Conditions"].str.contains("Partially cloudy"), 1, 0)
+#df["Overcast"] = np.where(df["Conditions"].str.contains("Overcast"), 1, 0)
 df.drop(columns="Conditions", inplace=True)
 
+
 # בחר את התכונות הרלוונטיות
-features_N = [ 'Minimum Temperature', 'Maximum Temperature','Temperature','Dew Point', 'Relative Humidity','Wind Direction','Wind Speed','Precipitation', 'Precipitation Cover','Snow Depth', 'Visibility', 'Cloud Cover', 'Sea Level Pressure']
+features_N = [ 'Minimum Temperature', 'Maximum Temperature','Temperature','Dew Point', 'Relative Humidity','Wind Speed','Precipitation', 'Precipitation Cover','Snow Depth', 'Visibility', 'Cloud Cover', 'Sea Level Pressure']
 
 
 scaler = sk.preprocessing.StandardScaler()
 
 column_transformer = sk.compose.ColumnTransformer([
-    ('num', scaler, features_N),
-    ('cat', 'passthrough', ['Overcast',"Partially cloudy","Rain"])  # Add 'Overcast' with 'passthrough' transformer
+    ('num', scaler, features_N)#,
+    #('cat', 'passthrough', ['Overcast',"Partially cloudy","Rain"])  # Add 'Overcast' with 'passthrough' transformer
 ], remainder='drop')
 pipeline = Pipeline([
     ('preprocess', column_transformer),
@@ -75,7 +80,30 @@ pipeline.fit(df)
 norm_data = pipeline.fit_transform(df)
 
 
+n_pca_data_a = pd.concat([pd.DataFrame(norm_data).iloc[:,:1], df["Date time"], df["Address"]], axis=1)
 
+p_n_pca = pd.pivot(n_pca_data_a, index = 'Address',columns = "Date time" )
+
+
+# הגדרת צבעים אוטומטיים
+colors = plt.cm.tab20(np.arange(len(df['Address'].unique())))
+
+# לולאה לכל עמודה
+for col in df.columns:
+    # סינון נתונים לא זמינים
+    df_filtered = df
+
+    # יצירת גרף
+    plt.figure(figsize=(10, 6))
+    for i, address in enumerate(df_filtered['Address'].unique()):
+        df_address = df_filtered.loc[df_filtered['Address'] == address]
+        plt.plot(df_address['Date time'], df_address[col], color=colors[i], label=address)
+    
+    # הגדרות גרף
+    plt.title(col)
+    plt.xlabel('תאריך')
+    plt.ylabel(col)
+    plt.show()
 
 
 
